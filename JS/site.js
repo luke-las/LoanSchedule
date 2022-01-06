75288345e5d103386d4b3fd6fb99189804d3f7c2
luke-las <84208824+luke-las@users.noreply.github.com>
2022-01-06 12:43:27 -0500

    formatting

--- JS/site.js
+++ JS/site.js
@@ -1,29 +1,32 @@
 function getValues(){
-    let loanBalance = (document.getElementById("loanBalance").value);
+    let loanBalance = Number(document.getElementById("loanBalance").value);
     let loanTerm = (document.getElementById("loanTerm").value);
-
     let loanInterest = (document.getElementById("loanInterest").value);
-        
+
     const rowObject= {};
+    rowObject.Payment = totalMonthly(loanBalance, loanInterest, loanTerm);
+    document.getElementById("totalPrincipal").innerHTML = loanBalance.toFixed(2);
+
     let totalInterest=0;
 
     rowObject.Balance = loanBalance;
+    let ogLoanBalance = Number(loanBalance);
     let templateRows ="";
     for(i = 1; i<=loanTerm; i++){
-        rowObject.Payment = totalMonthly(loanBalance, loanInterest, loanTerm);
         rowObject.Interest = interestPayment(loanBalance, loanInterest);
         totalInterest+=rowObject.Interest;
-        loanBalance = loanBalance - rowObject.Payment;
+        loanBalance = loanBalance - rowObject.Payment + rowObject.Interest;
         rowObject.Principal = principalPayment(rowObject.Payment, rowObject.Interest);
 
 
-        templateRows+=`<tr><th scope="row">${i}</th><td>${rowObject.Payment}</td><td>${rowObject.Principal}</td><td>${rowObject.Interest}</td><td>${totalInterest}</td><td>${loanBalance}</td></tr>`
+        templateRows+=`<tr><th scope="row">${i}</th><td>${rowObject.Payment.toFixed(2)}</td><td>${rowObject.Principal.toFixed(2)}</td><td>${rowObject.Interest.toFixed(2)}</td><td>${totalInterest.toFixed(2)}</td><td>${loanBalance.toFixed(2)}</td></tr>`
     }
+    
     document.getElementById("tableResults").innerHTML = templateRows;
-
-    document.getElementById("monthlyPayment").innerHTML = rowObject.Payment;
-    document.getElementById("totalPrincipal").innerHTML = loanBalance;
-    document.getElementById("totalCost").innerHTML = totalInterest + loanBalance;
+    document.getElementById("monthlyPayment").innerHTML = rowObject.Payment.toFixed(2);
+    document.getElementById("totalInterest").innerHTML = totalInterest.toFixed(2);
+    let totalCost = totalInterest + ogLoanBalance;
+    document.getElementById("totalCost").innerHTML = (totalInterest+ogLoanBalance).toFixed(2);
 }
 
 function principalPayment(Payment, Interest){
